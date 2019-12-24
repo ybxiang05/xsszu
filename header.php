72137d9173d803d8ac3ec3556d3c54d38c0a07d8
Brooke <brooke.xiang@gmail.com>
2019-12-23 21:51:33 -0800

    init

---
+++ header.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="page" class="hfeed site">
+			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+
+			<header id="masthead" class="site-header" role="banner">
+				
+
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<a class="xsszu-logo" href="<?php echo esc_url(home_url('/'))?>">
+						<img class="logo-img" src="https://via.placeholder.com/150" alt="xsszu site logo">
+					</a>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
+
+			<div id="content" class="site-content">
